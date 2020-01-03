eca028769013a352479347fe0902ca5b54efef18
Liam Kreiss <liam.kreiss@gmail.com>
2020-01-03 14:03:39 -0800

    added helper method for displaying conversions

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,14 @@ import javax.swing.JFrame;
 public class Main {
 
     public static void main(String[] args) {
-        Calculator test = new Calculator();
 //        String num1 = "^^v^0000", num2 = "0v0vv";
         String num1 = "v^00^", num2 = "vv^^v0v0vv";
 
-        String num1b10 = test.convertToBaseTen(num1);
-        String num2b10 = test.convertToBaseTen(num2);
+        String num1b10 = cleanConvertToBaseTen(num1);
+        String num2b10 = cleanConvertToBaseTen(num2);
 
-        System.out.println(test.convertToBaseTen(num1));
-        System.out.println(test.convertToBaseTen(num2));
-
-        System.out.println(test.convertToBalancedTernary(num1b10));
-        System.out.println(test.convertToBalancedTernary(num2b10));
+        String newNum1 = cleanConvertToBalancedTernary(num1b10);
+        String newNum2 = cleanConvertToBalancedTernary(num2b10);
 
 //        System.out.println(test.flip(num1));
 //        System.out.println(test.sum(test.flip(num2), num2));
@@ -30,6 +26,22 @@ public class Main {
         view.setLocation(100, 100);
         view.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         view.setVisible(true);
+    }
+
+    public static String cleanConvertToBaseTen(String b3num) {
+        Calculator test = new Calculator();
+        String converted = test.convertToBaseTen(b3num);
+        System.out.printf("Balanced ternary number: %s\n", b3num);
+        System.out.printf("Base ten number: %s\n\n", converted);
+        return converted;
+    }
+
+    public static String cleanConvertToBalancedTernary(String b10num) {
+        Calculator test = new Calculator();
+        String converted = test.convertToBalancedTernary(b10num);
+        System.out.printf("Base ten number: %s\n", b10num);
+        System.out.printf("Balanced ternary number: %s\n\n", converted);
+        return converted;
     }
 
 }
